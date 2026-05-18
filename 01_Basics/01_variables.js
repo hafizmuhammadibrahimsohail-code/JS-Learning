d209bb682851d30e4960ea43da8dd6597254ded6
hafizmuhammadibrahimsohail-code <hafizmuhammadibrahimsohail@gmail.com>
2026-05-18 17:33:23 +0000

    conifg

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "ibbi@123.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+// console.log(accountId);
+// prefer not to use var bcz of  issue in block scope and functional scope
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+// console.log(typeof string);
+
