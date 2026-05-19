d21fdfc3b7f4f096575d1328602a0f5fb446ad50
hafizmuhammadibrahimsohail-code <hafizmuhammadibrahimsohail@gmail.com>
2026-05-19 14:07:24 +0000

    comparision

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,37 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2); 
+// console.log(2-2); 
+// // console.log(2٭2); 
+// console.log(2/3); 
+// console.log(2٪3);
+
+
+let str1 = "Assalamualaikum "
+let str2 = "Ibrahim Sohail"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("2" + 1 + 1);
+// console.log(1 + 1 + "2");
+// console.log("5" + 4)
+// console.log(2 + 2 + 1 + "4")
+
+let gameCounter = 100;
+// console.log(gameCounter)
+gameCounter++;
+
+// console.log(gameCounter)
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+
+// === checks the datatype also
+console.log(2 === 2)
